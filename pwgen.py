e502c26bc48d9bcd2783a3bbfedc4501425fafdb
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-15 11:16:36 +0200

    Add the main function to print the generated password

--- pwgen.py
+++ pwgen.py
@@ -6,3 +6,11 @@ def generate_password(length: int = 16) -> str:
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(secrets.choice(characters) for _ in range(length))
     return password
+
+
+def main():
+    print(generate_password())
+
+
+if __name__ == '__main__':
+    main()
