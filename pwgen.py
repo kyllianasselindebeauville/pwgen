e8d1b820bcdf52c1fbcfdec85359a82fe6b63805
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-15 11:47:29 +0200

    Add a docstring to the `generate_password` function

--- pwgen.py
+++ pwgen.py
@@ -3,6 +3,16 @@ import string
 
 
 def generate_password(length: int = 16) -> str:
+    """Generates a password.
+
+    Generates a secure random password using the secrets module.
+
+    Args:
+        length: Length of the password to generate.
+
+    Returns:
+        The generated password as a str.
+    """
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(secrets.choice(characters) for _ in range(length))
     return password
