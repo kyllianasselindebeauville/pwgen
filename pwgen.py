064a0b665597e2030c092aad5f62b4767e5e2677
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-18 10:39:58 +0200

    Add symbols option

--- pwgen.py
+++ pwgen.py
@@ -6,7 +6,8 @@ import string
 def generate_password(length: int = 16,
                       lowercase: bool = False,
                       uppercase: bool = False,
-                      digits: bool = False) -> str:
+                      digits: bool = False,
+                      symbols: bool = False) -> str:
     """Generates a password.
 
     Generates a secure random password using the secrets module.
@@ -16,13 +17,15 @@ def generate_password(length: int = 16,
         lowercase: Whether or not to include lowercase letters in the password.
         uppercase: Whether or not to include uppercase letters in the password.
         digits: Whether or not to include digits in the password.
+        symbols: Whether or not to include symbols in the password.
 
     Returns:
         The generated password as a str.
     """
     characters = ((string.ascii_lowercase if lowercase else '') +
                   (string.ascii_uppercase if uppercase else '') +
-                  (string.digits if digits else '') or
+                  (string.digits if digits else '') +
+                  (string.punctuation if symbols else '') or
                   (string.ascii_letters + string.digits + string.punctuation))
     password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
@@ -38,10 +41,13 @@ def main():
                         help='include uppercase letters in the password')
     parser.add_argument('-d', '--digits', action='store_true',
                         help='include digits in the password')
+    parser.add_argument('-s', '--symbols', action='store_true',
+                        help='include symbols in the password')
 
     args = parser.parse_args()
     password = generate_password(length=args.length, lowercase=args.lowercase,
-                                 uppercase=args.uppercase, digits=args.digits)
+                                 uppercase=args.uppercase, digits=args.digits,
+                                 symbols=args.symbols)
     print(password)
 
 
