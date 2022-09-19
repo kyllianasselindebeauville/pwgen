5729e12f8b9fc82c4ac0828cfce80bb5c1637516
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-19 11:19:53 +0200

    Add help option explicitly

--- pwgen.py
+++ pwgen.py
@@ -32,11 +32,14 @@ def generate_password(length: int = 16,
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate a password.')
+    parser = argparse.ArgumentParser(description='Generate a password.',
+                                     add_help=False)
     parser.add_argument('length', nargs='?', default=16, type=int,
                         help='length of the password to generate')
     parser.add_argument('-d', '--digits', action='store_true',
                         help='include digits in the password')
+    parser.add_argument('-h', '--help', action='help',
+                        help='show this help message and exit')
     parser.add_argument('-l', '--lowercase', action='store_true',
                         help='include lowercase letters in the password')
     parser.add_argument('-s', '--symbols', action='store_true',
