3a8c78c96f8bf5994c362b4e05c1b28bcf317b4b
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-20 10:05:43 +0200

    Move the CLI to cli.py

--- pwgen/__main__.py
+++ pwgen/__main__.py
@@ -1,28 +1,9 @@
-import argparse
-
+from pwgen.cli import parse_args
 from pwgen.password import generate_password
-from pwgen.version import __version__
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate a password.',
-                                     add_help=False)
-    parser.add_argument('length', nargs='?', default=16, type=int,
-                        help='length of the password to generate')
-    parser.add_argument('-d', '--digits', action='store_true',
-                        help='include digits in the password')
-    parser.add_argument('-h', '--help', action='help',
-                        help='show this help message and exit')
-    parser.add_argument('-l', '--lowercase', action='store_true',
-                        help='include lowercase letters in the password')
-    parser.add_argument('-s', '--symbols', action='store_true',
-                        help='include symbols in the password')
-    parser.add_argument('-u', '--uppercase', action='store_true',
-                        help='include uppercase letters in the password')
-    parser.add_argument('-V', '--version', action='version',
-                        version=f'%(prog)s {__version__}')
-
-    args = parser.parse_args()
+    args = parse_args()
     password = generate_password(length=args.length, lowercase=args.lowercase,
                                  uppercase=args.uppercase, digits=args.digits,
                                  symbols=args.symbols)
