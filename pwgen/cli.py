34c52aef8f2658454e350e07ef14970a6a6845f8
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-20 11:11:31 +0200

    Simplify help messages

--- pwgen/cli.py
+++ pwgen/cli.py
@@ -10,17 +10,17 @@ def parse_args():
                                      add_help=False)
 
     parser.add_argument('length', nargs='?', default=16, type=int,
-                        help='length of the password to generate')
+                        help='length of the password')
     parser.add_argument('-d', '--digits', action='store_true',
-                        help='include digits in the password')
+                        help='include digits')
     parser.add_argument('-h', '--help', action='help',
                         help='show this help message and exit')
     parser.add_argument('-l', '--lowercase', action='store_true',
-                        help='include lowercase letters in the password')
+                        help='include lowercase letters')
     parser.add_argument('-s', '--symbols', action='store_true',
-                        help='include symbols in the password')
+                        help='include symbols')
     parser.add_argument('-u', '--uppercase', action='store_true',
-                        help='include uppercase letters in the password')
+                        help='include uppercase letters')
     parser.add_argument('-V', '--version', action='version',
                         version=f'%(prog)s {__version__}')
 
