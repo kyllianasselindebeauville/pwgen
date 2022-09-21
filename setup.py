04d168ee6b5a57e79c3467a058a89edfb7ad64ad
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-21 19:31:26 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+exec(open('pwgen/version.py').read())
+
+setup(
+    name='pwgen',
+    version=__version__,
+    description='A simple password generator',
+    author='Kyllian Asselin de Beauville',
+    author_email='kyllianasselindebeauville@gmail.com',
+    url='https://github.com/kyllianasselindebeauville/pwgen',
+    packages=['pwgen'],
+    license='MIT'
+)
