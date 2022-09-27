e6626f94637a91eea4c204553d760058a89f096f
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-27 21:50:12 +0200

    Pass the arguments to `generate_password` using a dict

--- pwgen/__main__.py
+++ pwgen/__main__.py
@@ -4,9 +4,7 @@ from pwgen.password import generate_password
 
 def main():
     args = parse_args()
-    password = generate_password(length=args.length, lowercase=args.lowercase,
-                                 uppercase=args.uppercase, digits=args.digits,
-                                 symbols=args.symbols)
+    password = generate_password(**vars(args))
     print(password)
 
 
