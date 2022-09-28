51f2a206ba7dbb739aba8a67a6a53a665f4e9d3e
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-09-28 11:32:25 +0200

    Update the description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ exec(open('pwgen/version.py').read())
 setup(
     name='pwgen',
     version=__version__,
-    description='A simple password generator',
+    description='A secure password generator',
     author='Kyllian Asselin de Beauville',
     author_email='kyllianasselindebeauville@gmail.com',
     url='https://github.com/kyllianasselindebeauville/pwgen',
